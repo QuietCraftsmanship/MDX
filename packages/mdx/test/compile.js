QuietCraftsmanship/MDX

commit 3e27483efe138649299e7ce68e10a0e09e654be7

Fix rewriting of components for custom elements
This commit particularly solves elements names that include dashes: custom
elements, such as `custom-element-name`.
These can be written in JSX like so:

```jsx
<custom-element-name />
```

…which is fine.

But to support passing them in, we were previously rewriting such code to a
member id, to take a key from an object, like so:

```jsx
<_components.custom-element-name>
```

…which crashed.

This commit solves that by taking the component from the object in a temporary
variable, and using that valid variable name as a single component name.

```js
const _component0 = _components['custom-element-name']

<_component0 />
```

Closes GH-2100.
Closes GH-2101.

Co-authored-by: Titus Wormer <tituswormer@gmail.com>

Reviewed-by: Titus Wormer <tituswormer@gmail.com>

packages/mdx/lib/plugin/recma-jsx-rewrite.js
@@ -72,6 +72,8 @@ export function recmaJsxRewrite(options = {}) {
     let createErrorHelper
     /** @type {Scope|null} */
     let currentScope
+    /** @type {Map<string | number, string>} */
+    const idToInvalidComponentName = new Map()
 
     walk(tree, {
       enter(_node) {
@@ -193,16 +195,24 @@ export function recmaJsxRewrite(options = {}) {
               fnScope.tags.push(id)
             }
 
-            node.openingElement.name = toJsxIdOrMemberExpression([
-              '_components',
-              id
-            ])
+            /** @type {Array<string | number>} */
+            let jsxIdExpression = ['_components', id]
+            if (isIdentifierName(id) === false) {
+              let invalidComponentName = idToInvalidComponentName.get(id)
+              if (invalidComponentName === undefined) {
+                invalidComponentName = `_component${idToInvalidComponentName.size}`
+                idToInvalidComponentName.set(id, invalidComponentName)
+              }
+
+              jsxIdExpression = [invalidComponentName]
+            }
+
+            node.openingElement.name =
+              toJsxIdOrMemberExpression(jsxIdExpression)
 
             if (node.closingElement) {
-              node.closingElement.name = toJsxIdOrMemberExpression([
-                '_components',
-                id
-              ])
+              node.closingElement.name =
+                toJsxIdOrMemberExpression(jsxIdExpression)
             }
           }
         }
@@ -259,7 +269,11 @@ export function recmaJsxRewrite(options = {}) {
           /** @type {Array<Statement>} */
           const statements = []
 
-          if (defaults.length > 0 || actual.length > 0) {
+          if (
+            defaults.length > 0 ||
+            actual.length > 0 ||
+            idToInvalidComponentName.size > 0
+          ) {
             if (providerImportSource) {
               importProvider = true
               parameters.push({
@@ -342,6 +356,27 @@ export function recmaJsxRewrite(options = {}) {
                 init: componentsInit
               })
               componentsInit = {type: 'Identifier', name: '_components'}
+            }
+
+            if (isNamedFunction(scope.node, '_createMdxContent')) {
+              for (const [id, componentName] of idToInvalidComponentName) {
+                // For JSX IDs that can’t be represented as JavaScript IDs (as in,
+                // those with dashes, such as `custom-element`), generate a
+                // separate variable that is a valid JS ID (such as `_component0`),
+                // and takes it from components:
+                // `const _component0 = _components['custom-element']`
+                declarations.push({
+                  type: 'VariableDeclarator',
+                  id: {type: 'Identifier', name: componentName},
+                  init: {
+                    type: 'MemberExpression',
+                    object: {type: 'Identifier', name: '_components'},
+                    property: {type: 'Literal', value: id},
+                    computed: true,
+                    optional: false
+                  }
+                })
+              }
             }
 
             if (componentsPattern) {

packages/mdx/test/compile.js
@@ -860,8 +860,8 @@ test('jsx', async () => {
       'function _createMdxContent(props) {',
       '  const _components = Object.assign({',
       '    "a-b": "a-b"',
-      '  }, props.components);',
-      '  return <>{<_components.a-b></_components.a-b>}</>;',
+      '  }, props.components), _component0 = _components["a-b"];',
+      '  return <>{<_component0></_component0>}</>;',
       '}',
       'function MDXContent(props = {}) {',
       '  const {wrapper: MDXLayout} = props.components || ({});',

commit a2271d6cbe79b410fdeca73c07c27ddea9b513e2

Fix bug with (injected) custom elements and layouts
This fixes a bug, uncovered in mdx-js/mdx#2112 and in mdx-js/mdx#2123,
which is rather unlikely to occur.
It only occurs when:

1.  Custom elements (such as `<a-b></a-b>`) are injected (not authored)
    into a tree
2.  Either:
    1.  A layout component is defined within an MDX document
    2.  A provider is used, and any component is defined within MDX
        documents

In those cases, an accidental `const ;` was injected into the final
serialized document.
Which caused anything trying to run the code to crash.

The problem was introduced in 3e27483, the commit message of which
sheds some light on why custom elements are peculiar and need extra
handling.

We track which component contains which other components.
If some component uses `<A />`, then some code to handle `A` is
injected in that component.
If a different component uses `<B />`, some code for `B` is injected
inside it.
But the components don’t need to know about what’s used in other
components.

This mechanism had a mistake for custom elements: they were tracked
globally.
This commit fixes that, by tracking them scoped to the component that
includes them.

Related-to: GH-2100.
Related-to: GH-2101.

Closes GH-2112.
Closes GH-2123.

Co-authored-by: Caleb Eby <caleb.eby01@gmail.com>
Co-authored-by: bholmesdev <hey@bholmes.dev>

packages/mdx/lib/plugin/recma-jsx-rewrite.js
@@ -32,6 +32,7 @@
  * @property {Array<string>} components
  * @property {Array<string>} tags
  * @property {Record<string, {node: JSXElement, component: boolean}>} references
+ * @property {Map<string|number, string>} idToInvalidComponentName
  * @property {ESFunction} node
  */
 
@@ -72,8 +73,6 @@ export function recmaJsxRewrite(options = {}) {
     let createErrorHelper
     /** @type {Scope|null} */
     let currentScope
-    /** @type {Map<string | number, string>} */
-    const idToInvalidComponentName = new Map()
 
     walk(tree, {
       enter(_node) {
@@ -92,6 +91,7 @@ export function recmaJsxRewrite(options = {}) {
             components: [],
             tags: [],
             references: {},
+            idToInvalidComponentName: new Map(),
             node
           })
 
@@ -198,10 +198,11 @@ export function recmaJsxRewrite(options = {}) {
             /** @type {Array<string | number>} */
             let jsxIdExpression = ['_components', id]
             if (isIdentifierName(id) === false) {
-              let invalidComponentName = idToInvalidComponentName.get(id)
+              let invalidComponentName =
+                fnScope.idToInvalidComponentName.get(id)
               if (invalidComponentName === undefined) {
-                invalidComponentName = `_component${idToInvalidComponentName.size}`
-                idToInvalidComponentName.set(id, invalidComponentName)
+                invalidComponentName = `_component${fnScope.idToInvalidComponentName.size}`
+                fnScope.idToInvalidComponentName.set(id, invalidComponentName)
               }
 
               jsxIdExpression = [invalidComponentName]
@@ -272,7 +273,7 @@ export function recmaJsxRewrite(options = {}) {
           if (
             defaults.length > 0 ||
             actual.length > 0 ||
-            idToInvalidComponentName.size > 0
+            scope.idToInvalidComponentName.size > 0
           ) {
             if (providerImportSource) {
               importProvider = true
@@ -359,7 +360,10 @@ export function recmaJsxRewrite(options = {}) {
             }
 
             if (isNamedFunction(scope.node, '_createMdxContent')) {
-              for (const [id, componentName] of idToInvalidComponentName) {
+              for (const [
+                id,
+                componentName
+              ] of scope.idToInvalidComponentName) {
                 // For JSX IDs that can’t be represented as JavaScript IDs (as in,
                 // those with dashes, such as `custom-element`), generate a
                 // separate variable that is a valid JS ID (such as `_component0`),
@@ -387,11 +391,13 @@ export function recmaJsxRewrite(options = {}) {
               })
             }
 
-            statements.push({
-              type: 'VariableDeclaration',
-              kind: 'const',
-              declarations
-            })
+            if (declarations.length > 0) {
+              statements.push({
+                type: 'VariableDeclaration',
+                kind: 'const',
+                declarations
+              })
+            }
           }
 
           /** @type {string} */

packages/mdx/test/compile.js
@@ -1173,6 +1173,35 @@ test('remark-rehype options', async () => {
   )
 })
 
+// See <https://github.com/mdx-js/mdx/issues/2112>
+test('should support custom elements with layouts', async () => {
+  assert.equal(
+    renderToStaticMarkup(
+      React.createElement(
+        await run(
+          await compile('export default function () {}', {
+            rehypePlugins: [
+              /** @type {import('unified').Plugin<[], import('hast').Root>} */
+              function () {
+                return function (tree) {
+                  tree.children.push({
+                    type: 'element',
+                    tagName: 'custom-element',
+                    properties: {},
+                    children: []
+                  })
+                }
+              }
+            ]
+          })
+        )
+      )
+    ),
+    '',
+    'should not crash if element names are used that are not valid JavaScript identifiers, with layouts'
+  )
+})
+
 test('MDX (JSX)', async () => {
   assert.equal(
     renderToStaticMarkup(